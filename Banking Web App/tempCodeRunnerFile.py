aacd5859f6dda60778ea67c37ccc350fde4927cf
Matidza M.Z <Matidza46@gmail.com>
2024-08-13 14:46:37 +0200

    projects update

---
+++ Banking Web App/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+app = Flask(__name__)
+if __name__ == "__main__":
+     port = int(os.environ.get("PORT", 5000))
+     app.run(debug=True, host='0.0.0.0', port=port)
--- Student Card (Django)/card.py
+++ Student Card (Django)/card.py
@@ -1,14 +1,14 @@
 class Student:
-    def __init__(self, name, surname, code, degree, skills, campus):
+    def __init__(self, name, surname, code, skills, campus):
         self.name = name
         self.surname = surname
         self.code = code
-        self.degree = degree
+
         self.skills = skills
         self.campus = campus
 
     def __str__(self):
-        return f"\nNames: {self.name} {self.surname} \nStudent No: {self.degree} \nDegree: {self.code}\nSkills: {self.skills} \nCampus: {self.campus}"
+        return f"\nNames: {self.name} {self.surname} \nStudent No:{self.code}\nSkills: {self.skills} \nCampus: {self.campus}"
     #name
     @property
     def name(self):
@@ -53,14 +53,7 @@ class Student:
         if not isinstance(degree, str): raise TypeError("Degree must be in letters!")  
         else:
             self.__degree = degree
-    #Skills
-    @property
-    def skills(self):
-        return self.__skills
 
-    @skills.setter
-    def skills(self, skills):
-        self.__skills = skills
 
     @property
     def campus(self):
@@ -78,13 +71,13 @@ def main():
     print(student_card)
 
 def get_card():
-    name = input("What is your name: ")
-    surname = input("What is your surname: ")
+    name = input("Name? ")
+    surname = input("Surname? ")
     degree = input("What degree are you enrolled in: ")
     code = int(input("Student number: "))
-    skills = input("Skills(Space-Seperated): ").title().split()
+    
     campus = input("Campus: ").title()
-    return Student(name, surname, degree, code, skills, campus)
+    return Student(name, surname, degree, code, campus)
 
 if __name__ == "__main__":
     main()        
